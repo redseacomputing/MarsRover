3b83c7212cd8c1e5119ee0c0059a104d82c3d6eb
christopher <christopher.zelezny@mailbox.org>
2021-02-24 18:54:19 +0100

    added newDirection in level2

--- src/main/java/Level2.java
+++ src/main/java/Level2.java
@@ -1,2 +1,28 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Level2 {
+
+    private final int MAXIMUM_POSITIVE_STEERING_ANGLE = 45;
+    private final int MAXIMUM_NEGATIVE_STEERING_ANGLE = MAXIMUM_POSITIVE_STEERING_ANGLE * -1;
+
+    private double calculateTurnRadiusFrom(double wheelBase, double steeringAngle) {
+        double inRadians = Math.toRadians(steeringAngle);
+        return roundResult((wheelBase / Math.sin(inRadians)), 2);
+    }
+
+    public double calculateNewDirectionFrom(double wheelBase, double distance, double steeringAngle){
+        double radius = calculateTurnRadiusFrom(wheelBase, steeringAngle);
+        double newDirection = (distance * 360) / (2 * radius * Math.PI);
+        return roundResult(newDirection,2);
+    }
+
+    private double roundResult(double value, int places) {
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+
+
 }
