527804b964c199b1502ef6aab63fc0712457fed5
christopher <christopher.zelezny@mailbox.org>
2021-02-25 21:16:13 +0100

    added X Coordinate functionality

--- src/main/java/Level2.java
+++ src/main/java/Level2.java
@@ -3,28 +3,45 @@ import java.math.RoundingMode;
 
 public class Level2 {
 
-    private final int MAXIMUM_POSITIVE_STEERING_ANGLE = 45;
-    private final int MAXIMUM_NEGATIVE_STEERING_ANGLE = MAXIMUM_POSITIVE_STEERING_ANGLE * -1;
+    private final double MAXIMUM_POSITIVE_STEERING_ANGLE = 45d;
+    private final double MAXIMUM_NEGATIVE_STEERING_ANGLE = MAXIMUM_POSITIVE_STEERING_ANGLE * -1;
+    private final double IMPOSSIBLE_STEERING_ANGLE_TO_CALCULATE = 0d;
 
-    public double calculateNewDirectionFrom(double wheelBase, double distance, double steeringAngle){
+    public double calculateNewDirectionFrom(double wheelBase, double distance, double steeringAngle) {
         double radius = calculateTurnRadiusFrom(wheelBase, steeringAngle);
+        if(radius == IMPOSSIBLE_STEERING_ANGLE_TO_CALCULATE) return 0d;
         double newDirection = (distance * 360) / (2 * radius * Math.PI);
-        return roundResult(newDirection,2);
+        if (newDirection > 0) {
+            while (newDirection > 360) {
+                newDirection -= 360;
+            }
+        }
+        if (newDirection < 0) {
+            while (newDirection < -360) {
+                newDirection += 360;
+            }
+        }
+        return roundResult(newDirection, 2);
     }
 
-    public double calculateYCoordinatesFrom(double wheelBase, double distance,double steeringAngle){
+    public double calculateYCoordinatesFrom(double wheelBase, double distance, double steeringAngle) {
         double radius = calculateTurnRadiusFrom(wheelBase, steeringAngle);
+        if(radius == IMPOSSIBLE_STEERING_ANGLE_TO_CALCULATE) return distance;
         double newDirection = (distance * 360) / (2 * radius * Math.PI);
         double betaInDegrees = Math.abs(newDirection + 90d - 180d);
         double yCoordinate = Math.cos(Math.toRadians(betaInDegrees)) * radius;
-        return roundResult(yCoordinate,2);
+        return roundResult(yCoordinate, 2);
     }
 
-    public double calculateXCoordinatesFrom(double wheelBase, double distance,double steeringAngle){
-        return -1d;
+    public double calculateXCoordinatesFrom(double wheelBase, double distance, double steeringAngle) {
+        double radius = calculateTurnRadiusFrom(wheelBase, steeringAngle);
+        if(radius == IMPOSSIBLE_STEERING_ANGLE_TO_CALCULATE) return 0d;
+        double xCoordinate = roundResult(Math.sqrt(Math.pow(radius, 2) - Math.pow(calculateYCoordinatesFrom(wheelBase, distance, steeringAngle), 2)) - radius, 2);
+        return Math.abs(xCoordinate);
     }
 
     private double calculateTurnRadiusFrom(double wheelBase, double steeringAngle) {
+        if (steeringAngle == IMPOSSIBLE_STEERING_ANGLE_TO_CALCULATE) return 0d;
         double inRadians = Math.toRadians(steeringAngle);
         return roundResult((wheelBase / Math.sin(inRadians)), 2);
     }
